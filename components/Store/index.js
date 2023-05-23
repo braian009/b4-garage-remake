5ac76848065918ce9dfa5f823d4021a53da98063
braian <braianyanquin@gmail.com>
2023-05-22 22:28:58 -0300

    store card's style enhanced

--- components/Store/index.js
+++ components/Store/index.js
@@ -13,7 +13,20 @@ const Store = () => {
               <Image src={"/spoiler.jpg"} fill alt={""} />
             </div>
             <div className={styles.storeCardFooter}>
-              <button>shop this product</button>
+              <div>shop this product</div>
+              <div>
+                <svg
+                  fill="#000000"
+                  width="800px"
+                  height="800px"
+                  viewBox="0 0 1920 1920"
+                >
+                  <g fillRule="evenodd">
+                    <path d="M0 176.142 176.13.012l960.12 960.119L176.13 1920 0 1743.87l783.864-783.739L0 176.142Z" />
+                    <path d="M784 176.142 960.13.012l960.12 960.119L960.13 1920 784 1743.87l783.86-783.739L784 176.142Z" />
+                  </g>
+                </svg>
+              </div>
             </div>
           </div>
           <div className={styles.storeCard}>
@@ -21,7 +34,20 @@ const Store = () => {
               <Image src={"/wheel.jpg"} fill alt={""} />
             </div>
             <div className={styles.storeCardFooter}>
-              <button>shop this product</button>
+              <div>shop this product</div>
+              <div>
+                <svg
+                  fill="#000000"
+                  width="800px"
+                  height="800px"
+                  viewBox="0 0 1920 1920"
+                >
+                  <g fillRule="evenodd">
+                    <path d="M0 176.142 176.13.012l960.12 960.119L176.13 1920 0 1743.87l783.864-783.739L0 176.142Z" />
+                    <path d="M784 176.142 960.13.012l960.12 960.119L960.13 1920 784 1743.87l783.86-783.739L784 176.142Z" />
+                  </g>
+                </svg>
+              </div>
             </div>
           </div>
           <div className={styles.storeCard}>
@@ -29,7 +55,20 @@ const Store = () => {
               <Image src={"/oil.jpg"} fill alt={""} />
             </div>
             <div className={styles.storeCardFooter}>
-              <button>shop this product</button>
+              <div>shop this product</div>
+              <div>
+                <svg
+                  fill="#000000"
+                  width="800px"
+                  height="800px"
+                  viewBox="0 0 1920 1920"
+                >
+                  <g fillRule="evenodd">
+                    <path d="M0 176.142 176.13.012l960.12 960.119L176.13 1920 0 1743.87l783.864-783.739L0 176.142Z" />
+                    <path d="M784 176.142 960.13.012l960.12 960.119L960.13 1920 784 1743.87l783.86-783.739L784 176.142Z" />
+                  </g>
+                </svg>
+              </div>
             </div>
           </div>
         </div>
