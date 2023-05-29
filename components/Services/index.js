79811361706cec084e921d20a0e2a653058800ef
braian <braianyanquin@gmail.com>
2023-05-28 22:05:33 -0300

    sponsors icons added to services section

--- components/Services/index.js
+++ components/Services/index.js
@@ -1,29 +1,63 @@
 import * as React from "react";
-import styles from './services.module.css'
+import styles from "./services.module.css";
 
 const Services = () => {
   return (
     <div className={styles.container}>
       <div className={styles.inner}>
-        <div className={styles.serviceCard}>
-          <h2>some title</h2>
-          <p>
-            Nostrud ea adipisicing mollit amet consequat quis aute cupidatat
-            duis nisi ipsum non. Reprehenderit exercitation est aliquip deserunt
-            veniam ex qui elit et amet aliqua nostrud excepteur. Eu cupidatat
-            sit irure fugiat amet proident officia velit qui culpa tempor
-            tempor.
-          </p>
+        <div className={styles.servicesCards}>
+          <div className={styles.serviceCard}>
+            <h2>some title</h2>
+            <p>
+              Nostrud ea adipisicing mollit amet consequat quis aute cupidatat
+              duis nisi ipsum non. Reprehenderit exercitation est aliquip
+              deserunt veniam ex qui elit et amet aliqua nostrud excepteur.
+            </p>
+          </div>
+          <div className={styles.serviceCard}>
+            <h2>some title</h2>
+            <p>
+              Nostrud ea adipisicing mollit amet consequat quis aute cupidatat
+              duis nisi ipsum non. Reprehenderit exercitation est aliquip
+              deserunt veniam ex qui elit et amet aliqua nostrud excepteur.
+            </p>
+          </div>
         </div>
-        <div className={styles.serviceCard}>
-          <h2>some title</h2>
-          <p>
-            Nostrud ea adipisicing mollit amet consequat quis aute cupidatat
-            duis nisi ipsum non. Reprehenderit exercitation est aliquip deserunt
-            veniam ex qui elit et amet aliqua nostrud excepteur. Eu cupidatat
-            sit irure fugiat amet proident officia velit qui culpa tempor
-            tempor.
-          </p>
+        <div className={styles.sponsors}>
+          <p>powered by</p>
+          <ul>
+            <li>
+              <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none">
+                <path
+                  d="M11.0605 2.93203C11.3983 2.68689 11.5672 2.56432 11.7518 2.51696C11.9148 2.47514 12.0858 2.47514 12.2488 2.51696C12.4334 2.56432 12.6023 2.68689 12.9401 2.93203L21.0586 8.82396C21.397 9.06956 21.5663 9.19235 21.6686 9.3535C21.7589 9.49579 21.8119 9.65862 21.8224 9.82684C21.8344 10.0174 21.7697 10.2162 21.6404 10.6138L18.5401 20.1449C18.4109 20.5421 18.3463 20.7407 18.2247 20.8876C18.1173 21.0173 17.979 21.1178 17.8224 21.1798C17.6451 21.25 17.4362 21.25 17.0186 21.25H6.98203C6.56437 21.25 6.35554 21.25 6.17822 21.1798C6.02164 21.1178 5.88325 21.0173 5.77589 20.8876C5.65429 20.7407 5.58969 20.5421 5.4605 20.1449L2.36021 10.6138C2.23086 10.2162 2.16619 10.0174 2.17817 9.82684C2.18874 9.65862 2.24166 9.49579 2.33202 9.3535C2.43434 9.19235 2.60355 9.06956 2.94196 8.82396L11.0605 2.93203Z"
+                  stroke="#808080"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </li>
+            <li>
+              <svg
+                fill="#808080"
+                width="800px"
+                height="800px"
+                viewBox="0 0 56 56"
+              >
+                <path d="M 13.7851 49.5742 L 42.2382 49.5742 C 47.1366 49.5742 49.5743 47.1367 49.5743 42.3086 L 49.5743 13.6914 C 49.5743 8.8633 47.1366 6.4258 42.2382 6.4258 L 13.7851 6.4258 C 8.9101 6.4258 6.4257 8.8398 6.4257 13.6914 L 6.4257 42.3086 C 6.4257 47.1602 8.9101 49.5742 13.7851 49.5742 Z M 13.8554 45.8008 C 11.5117 45.8008 10.1992 44.5586 10.1992 42.1211 L 10.1992 13.8789 C 10.1992 11.4414 11.5117 10.1992 13.8554 10.1992 L 42.1679 10.1992 C 44.4882 10.1992 45.8007 11.4414 45.8007 13.8789 L 45.8007 42.1211 C 45.8007 44.5586 44.4882 45.8008 42.1679 45.8008 Z" />
+              </svg>
+            </li>
+            <li>
+              <svg
+                fill="#808080"
+                width="800px"
+                height="800px"
+                viewBox="0 0 56 56"
+              >
+                <path d="M 7.5860 42.9414 L 23.8516 52.1758 C 26.6407 53.7695 29.3126 53.7930 32.1485 52.1758 L 48.4141 42.9414 C 50.5938 41.6992 51.7890 40.4336 51.7890 37.0352 L 51.7890 18.8008 C 51.7890 15.4961 50.5703 14.3008 48.5783 13.1523 L 32.2657 3.8711 C 29.3595 2.2070 26.5704 2.2305 23.7344 3.8711 L 7.4219 13.1523 C 5.4297 14.3008 4.2110 15.4961 4.2110 18.8008 L 4.2110 37.0352 C 4.2110 40.4336 5.4063 41.6992 7.5860 42.9414 Z M 9.7891 39.6601 C 8.4532 38.9101 8.0079 38.1836 8.0079 36.9179 L 8.0079 18.9648 C 8.0079 17.7930 8.4766 17.1133 9.6485 16.4336 L 25.3985 7.4101 C 27.1797 6.4023 28.7735 6.3554 30.6016 7.4101 L 46.3516 16.4336 C 47.5237 17.1133 47.9922 17.7930 47.9922 18.9648 L 47.9922 36.9179 C 47.9922 38.1836 47.5468 38.9101 46.2110 39.6601 L 30.6016 48.5430 C 28.7266 49.5976 27.2032 49.5742 25.3985 48.5430 Z" />
+              </svg>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
