6b4afd2f54292a6b0d6d168692d77f6f69913753
braian <braianyanquin@gmail.com>
2023-05-31 22:30:26 -0300

    events page re-estructured and re-designed, cards on it changed as well

--- components/EventCard/index.js
+++ components/EventCard/index.js
@@ -1,16 +1,17 @@
 import * as React from "react";
+import Image from "next/image";
 import styles from "./eventCard.module.css";
 
-const EventCard = ({ title, description, date }) => {
+const EventCard = ({ title, description, date, image }) => {
   return (
     <div className={styles.cardContainer}>
       <div className={styles.cardDate}>
         <div>{date.month}</div>
         <div>{date.day}</div>
       </div>
+      <div className={styles.cardImage}><Image src={image} alt={''} fill/></div>
       <div className={styles.cardInfo}>
         <h4>{title}</h4>
-        <p>{description}</p>
       </div>
     </div>
   );
--- pages/events/index.js
+++ pages/events/index.js
@@ -4,30 +4,43 @@ import styles from "./events.module.css";
 import { eventList } from "@/data/events";
 import EventCard from "@/components/EventCard";
 import GetInfoForm from "@/components/GetInfoForm";
+import EventCardd from "@/components/EventCard";
+import Slogan from "@/components/Hero/Slogan";
 
 const Events = ({}) => {
   return (
     <div className={styles.container}>
-      <Image src={"/eventsbg.jpg"} alt={""} fill />
+      <Image src={"/eventsbgg.jpg"} alt={""} fill />
       <div className={styles.eventsOverlay}></div>
 
       <div className={styles.inner}>
-        <h1>Events</h1>
+        <div className={styles.eventsText}>
+          <h1>Events</h1>
+          <p>
+            Ipsum eiusmod quis enim duis minim excepteur ea pariatur incididunt
+            Lorem. Sunt culpa minim adipisicing excepteur do id id in minim quis
+            incididunt laboris incididunt. Consequat non magna quis quis sunt
+            tempor in.
+          </p>
+        </div>
         <div className={styles.eventCards}>
+          <Slogan />
+
           {eventList.map((event) => {
             return (
-              <EventCard
+              <EventCardd
                 key={`event-${event.id}`}
                 title={event.title}
                 description={event.description}
                 date={event.date}
+                image={event.image}
               />
             );
           })}
         </div>
-        <div className={styles.eventsForm}>
-          <GetInfoForm />
-        </div>
+      </div>
+      <div className={styles.eventsForm}>
+        <GetInfoForm />
       </div>
     </div>
   );
