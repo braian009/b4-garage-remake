09e8e4bb13936556847ee838db72668c5d6f3c10
braian <braianyanquin@gmail.com>
2023-06-07 23:52:00 -0300

    store page's basic concept added

---
+++ data/store/index.js
@@ -0,0 +1,68 @@
+export const productsList = {
+  wheels: [
+    {
+      id: 0,
+      name: "Product 0",
+      image: "/wheel.png",
+      details:
+        "Cillum officia minim sunt incididunt excepteur sit reprehenderit nulla culpa in laborum aliqua.",
+      price: "44.44",
+    },
+    {
+      id: 1,
+      name: "Product 1",
+      image: "/wheel.png",
+      details:
+        "Cillum officia minim sunt incididunt excepteur sit reprehenderit nulla culpa in laborum aliqua.",
+      price: "44.44",
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      image: "/wheel.png",
+      details:
+        "Cillum officia minim sunt incididunt excepteur sit reprehenderit nulla culpa in laborum aliqua.",
+      price: "44.44",
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      image: "/wheel.png",
+      details:
+        "Cillum officia minim sunt incididunt excepteur sit reprehenderit nulla culpa in laborum aliqua.",
+      price: "44.44",
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      image: "/wheel.png",
+      details:
+        "Cillum officia minim sunt incididunt excepteur sit reprehenderit nulla culpa in laborum aliqua.",
+      price: "44.44",
+    },
+    {
+      id: 5,
+      name: "Product 5",
+      image: "/wheel.png",
+      details:
+        "Cillum officia minim sunt incididunt excepteur sit reprehenderit nulla culpa in laborum aliqua.",
+      price: "44.44",
+    },
+    {
+      id: 6,
+      name: "Product 6",
+      image: "/wheel.png",
+      details:
+        "Cillum officia minim sunt incididunt excepteur sit reprehenderit nulla culpa in laborum aliqua.",
+      price: "44.44",
+    },
+    {
+      id: 7,
+      name: "Product 7",
+      image: "/wheel.png",
+      details:
+        "Cillum officia minim sunt incididunt excepteur sit reprehenderit nulla culpa in laborum aliqua.",
+      price: "44.44",
+    },
+  ],
+};
---
+++ pages/store/index.js
@@ -0,0 +1,40 @@
+import * as React from "react";
+import styles from "./store.module.css";
+import { productsList } from "@/data/store";
+import Image from "next/image";
+
+const Store = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.inner}>
+        <div className={styles.storeText}>
+          <h1>B4&apos;s Store</h1>
+        </div>
+        <div className={styles.storeNav}>
+          <ul>
+            <li>Fluids</li>
+            <li>Spoilers</li>
+            <li>Wheels</li>
+            <li>Accesories</li>
+          </ul>
+        </div>
+        <div className={styles.storeCards}>
+          {productsList.wheels.map((product) => {
+            return (
+              <div key={`product-${product.id}`} className={styles.storeCard}>
+                <div className={styles.cardImage}>
+                  <Image src={product.image} fill alt={""} />
+                </div>
+                <div className={styles.cardText}>
+                  <h4>{product.name}</h4>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Store;
